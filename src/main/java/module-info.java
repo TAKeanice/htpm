e5b6e0a583060d8b5bdc4678fb9eed35dbea1c2e
Tassilo Karge <tassilo.karge@uni-konstanz.de>
2019-04-18 23:40:11 +0200

    bumped target to java12 and made a proper jigsaw module
    probably to have a clean module interface some more tweaks are necessary - so far it works however.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module de.dbvis.htpm {
+    requires java.xml;
+    requires java.desktop;
+    requires org.apache.commons.lang3;
+
+    exports de.dbvis.htpm;
+    exports de.dbvis.htpm.constraints;
+    exports de.dbvis.htpm.db;
+    exports de.dbvis.htpm.hes;
+    exports de.dbvis.htpm.hes.events;
+    exports de.dbvis.htpm.htp;
+    exports de.dbvis.htpm.occurrence;
+    exports de.dbvis.htpm.util;
+    exports de.dbvis.htpm.htp.eventnodes;
+}
