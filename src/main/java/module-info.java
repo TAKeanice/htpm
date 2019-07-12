f2a2fbfd9e7827c94046909317dd3762675456df
Tassilo Karge <tassilo.karge@uni-konstanz.de>
2019-07-12 17:14:58 +0200

    added opens statements for all modules to allow reflection from other modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,16 @@ module de.dbvis.htpm {
     requires java.desktop;
     requires org.apache.commons.lang3;
 
+    opens de.dbvis.htpm;
+    opens de.dbvis.htpm.occurrence;
+    opens de.dbvis.htpm.constraints;
+    opens de.dbvis.htpm.db;
+    opens de.dbvis.htpm.hes;
+    opens de.dbvis.htpm.hes.events;
+    opens de.dbvis.htpm.htp;
+    opens de.dbvis.htpm.util;
+    opens de.dbvis.htpm.htp.eventnodes;
+
     exports de.dbvis.htpm;
     exports de.dbvis.htpm.occurrence;
     exports de.dbvis.htpm.constraints;
